7b2648696b0603add159e436ed1dd13d8ee1009e
Juliana <juliana@grandsinformatica.com.br>
2013-09-20 13:41:43 -0300

    consulta

--- src/test/java/br/com/sample/prevayler/EmpresaFacadeTest.java
+++ src/test/java/br/com/sample/prevayler/EmpresaFacadeTest.java
@@ -11,11 +11,19 @@ public class EmpresaFacadeTest {
 
         Prevayler<Empresa> prevayler = PrevaylerContext.getInstance();
         prevayler.execute(new CadastroFuncionario("José da Silva Jr"));
+        prevayler.execute(new CadastroFuncionario("José"));
+        prevayler.execute(new CadastroFuncionario("José Jr"));
+        prevayler.execute(new CadastroFuncionario("Jr"));
         prevayler.execute(new CadastroLoja("Loja da maria"));
 
         Empresa empresa = prevayler.prevalentSystem();
 
-        Assert.assertEquals(5, empresa.getFuncionarios().size());
-        Assert.assertEquals(3, empresa.getLojas().size());
+
+        Assert.assertEquals(4, empresa.getFuncionarios().size());
+        Assert.assertEquals(1, empresa.getLojas().size());
+
+        Assert.assertEquals(3, ConsultaNomeFuncionario.consultaNome("José").size());
+        Assert.assertEquals(1, ConsultaNomeFuncionario.consultaNome("Silva").size());
+        Assert.assertEquals(3, ConsultaNomeFuncionario.consultaNome("Jr").size());
     }
 }
